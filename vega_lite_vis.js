fc7d98d9b39e51206d86af46931795090b426740
chrischenwastaken <che0439200316@gmail.com>
2024-10-06 20:00:40 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -2,3 +2,8 @@ var vg_1 = "Week9Map.vg.json";
 vegaEmbed("#bar_chart", vg_1).then(function(result) {
 
 }).catch(console.error);
+
+var vg_1 = "Week10Map.vg.json";
+vegaEmbed("#line_chart", vg_1).then(function(result) {
+
+}).catch(console.error);
